1a32c56befd7c3f19b83716c3266353874b4d207
Mateo Jimenez <tidy-scorer-stem@duck.com>
2025-12-16 13:34:48 -0800

    feat(Gun.cs): Added tooltips for all inspector variables
     BREAKING CHANGE: Replaced all references to "bullet" to "projectile" to accommodate more generic use cases.

--- Runtime/Gun.cs
+++ Runtime/Gun.cs
@@ -5,13 +5,18 @@ using UnityEngine.InputSystem;
 public class Gun : MonoBehaviour
 {
     [Header("Projectile Settings")]
-    [SerializeField] GameObject bulletPrefab;
+    [Tooltip("The GameObject to instantiate upon shooting.")]
+    [SerializeField] GameObject projectilePrefab;
+    [Tooltip("The transform to instantiate the projectiles at. Note that the transform will rotate in order provide projectile spread, the object shouldnt be visible because of this.")]
     [SerializeField] Transform spawnPoint;
-    [SerializeField] float bulletSpeed = 10;
+    [Tooltip("The force to apply to the projectiles upon instatiation.")]
+    [SerializeField] float projectileSpeed = 10;
     [Header("Shooting Settings")]
     [Tooltip("The amount of projectiles to spawn when the DoShoot function is called.")]
-    [SerializeField] int bulletsPerShot = 1;
+    [SerializeField] int projectilesPerShot = 1;
+    [Tooltip("Applies random deviation of a fired projectile from the exact aim point. Example: spread = 1, projectile will can go between -1 - 1 degree off target.")]
     [SerializeField] float spread;
+    [Tooltip("The minimum amount of seconds that must pass before another projectile can be fired.")]
     [SerializeField] float shootingCooldown = 0.25f;
     [Tooltip("If enabled, holding down the shoot button will keep automatically firing projectiles.")]
     [SerializeField] bool allowButtonHolding;
@@ -21,7 +26,7 @@ public class Gun : MonoBehaviour
     private const string SHOOTACTIONSTRING = "Attack";
     private void OnValidate()
     {
-        if(bulletsPerShot < 0)
+        if(projectilesPerShot < 0)
         {
             Debug.LogError("Shots Per Click cannot be below 0!!");
         }
@@ -65,13 +70,13 @@ public class Gun : MonoBehaviour
     private IEnumerator DoShoot()
     {
         onCooldown = true;
-        for(int i = 0; i < bulletsPerShot; i++)
+        for(int i = 0; i < projectilesPerShot; i++)
         {
             spawnPoint.localRotation = Quaternion.Euler(new Vector3(spawnPoint.localRotation.x, spawnPoint.localRotation.y,Random.Range(-spread,spread)));
-            GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
-            if (bullet.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+            GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation);
+            if (projectile.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
             {
-                rb.linearVelocity = spawnPoint.up * bulletSpeed;
+                rb.linearVelocity = spawnPoint.up * projectileSpeed;
             }
         }
         yield return new WaitForSeconds(shootingCoolDown);
@@ -80,12 +85,12 @@ public class Gun : MonoBehaviour
 
     public void SetBulletSpeed(float amount)
     {
-        bulletSpeed = amount;
+        projectileSpeed = amount;
     }
 
     public void SetBulletsPerShot(float amount)
     {
-        bulletsPerShot = amount;
+        projectilesPerShot = amount;
     }
 
     public void SetSpread(float amount)
